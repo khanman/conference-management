853b59185b77102efd62416a28bc3a2a2e139a1a
mansoor <mansoorkhan07@gmail.com>
2015-03-13 18:11:23 -0400

    Webservice

--- public/js/client.js
+++ public/js/client.js
@@ -1,32 +1,38 @@
 ﻿var app = angular.module("DeveloperApp", []);
-app.controller("DeveloperController", function ($scope,$http) {
-    $http.get("/rest/website")
-    .success(function (response) {
+
+app.factory('WebsiteService', function ($http) {
+    var findAll = function (callback) {
+     $http.get("/rest/website")
+    .success(callback);
+    }
+    var remove = function(_id,callback)
+    {
+        $http.delete("/rest/website/" + _id)
+            .success(callback);
+    }
+    var create = function (name,callback) {
+        $http.post("/rest/website", name)
+        .success(callback);
+    }
+    return {
+        create: create,
+        findAll: findAll,
+        remove:remove
+    }
+});
+app.controller("DeveloperController", function ($scope, $http, WebsiteService) {
+    WebsiteService.findAll(function (response) {
         $scope.websites = response;
     });
+
     $scope.remove = function (_id) {
-        $http.delete("/rest/website/" + _id)
-         .success(function (response) {
-             $scope.websites = response;
-         });
+        WebsiteService.remove(_id, function (response) {
+            $scope.websites = response;
+        });
     }
     $scope.add = function (name) {
-        $http.post("/rest/website", name)         
-        .success(function (response) {
+        WebsiteService.create(name,function (response) {
             $scope.websites = response;
-            });
+        });
     }
-    //$scope.remove = function (index) {
-    //    console.log(index);
-    //  $http.delete("/rest/dev/" + index)
-    //   .success(function (response) {
-    //        $scope.dev = response;
-    //    });
-    //}
-    
-    //$scope.select = function ($index) {
-    //    $scope.selectedIndex = $index;
-    //    $scope.applications = $scope.dev[$index].apps;
- 
-    //}
 });
